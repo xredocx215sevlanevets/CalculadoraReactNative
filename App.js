d5f15a5ffa4b3becdc7abad5331defc3a475463a
xredocx215sevlanevets <stevenalves512@gmail.com>
2020-01-14 16:24:40 -0100

    create constructor, handleOp function, onPress event on t buttom and make the display show data

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      display: '',
+      result: '',
+    }
+  }
+
+  handleOp(op) {
+    this.setState({
+      display: this.state.display + op
+    })
+  }
+
+  render() {
     const col1Buttons = [
       ['7', '8', '9'],
       ['4', '5', '6'],
@@ -16,28 +30,30 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
 
-        <Text style={styles.display}>Display</Text>
-        <Text style={styles.result}>Result</Text>
+        <Text style={styles.display}>{this.state.display}</Text>
+        <Text style={styles.result}>{this.state.result}</Text>
 
         <View style={styles.buttons}>
+
           <View style={styles.col1}>
             {col1Buttons.map((line, ind) => <View key={ind} style={styles.line}>
-              {line.map(op => <View key={op} style={styles.btn}>
+              {line.map(op => <TouchableOpacity key={op} style={styles.btn} onPress={() => this.handleOp(op)}>
                 <Text style={styles.btnText}>
                   {op}
                 </Text>
-              </View>)}
+              </TouchableOpacity>)}
             </View>
             )}
           </View>
 
           <View style={styles.col2}>
-            {col2Buttons.map(op => <View key={op} style={styles.btn}>
+            {col2Buttons.map(op => <TouchableOpacity key={op} style={styles.btn} onPress={() => this.handleOp(op)}>
               <Text style={styles.btnText}>
                 {op}
               </Text>
-            </View>)}
+            </TouchableOpacity>)}
           </View>
+
         </View>
 
       </View>
